6fb7399a3503a62b1e866976d45ee7617c0e86f7
inofomoh9 <inofomohabdulshakur4@gmail.com>
2025-07-26 12:30:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Mindlift-/',
+  base: '/base: '/Mindlift-fixed-/', // ✅ must match your repo name exactly
   plugins: [react()]
 });
